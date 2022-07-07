43ba9ca220f32d7f7da72b30013a074ccf3b870d
Amdrii <andreyk557@gmail.com>
2022-07-07 15:52:21 +0300

    nuv/fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,18 @@
 
 $(document).ready(function(){
-
     $('.owl-carousel').owlCarousel({
         loop:true,
-        Boolean:true,
-        Number:50,
         margin:0,
         nav:true,
+        pause: 'false',
         navText: [ '', ' ' ],
-
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplaySpeed: 1500,
+        autoplayHoverPause: 0,
+        navSpeed: 3000,
         responsive:{
-            0:{
-                items:1
-            },
-
-            1000:{
+             0:{
                 items:1
             }
         }
